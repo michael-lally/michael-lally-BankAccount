d747fe1a17e17e446b8e5dfdeaa1fcce9ceae74f
michael-lally <mplally@neiu.edu>
2023-03-03 22:29:35 +0000

    Completed BankAccount

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance += amount;
     }
 
     /**
@@ -32,6 +32,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+        if(balance - amount < 0)
+            System.out.print("insufficient funds");
+        else
+            balance -= amount;
+
 
     }
 
@@ -40,6 +45,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
